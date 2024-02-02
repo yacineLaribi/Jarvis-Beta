59ddf3dc01bc52deb04264e6dc5c892366c689dd
unknown <lyacine230@gmail.com>
2024-02-03 00:12:56 +0100

    adding comments

--- jarvis.py
+++ jarvis.py
@@ -68,7 +68,7 @@ if __name__ == "__main__":
     time.sleep(2)
     while True:
         query = takecom().lower()
-
+        #Searching in wikipedia summary
         if "what do you know about" in query or "wikipedia" in query or "search" in query:
             speak("searching details....Wait")
             query.replace("wikipedia","")
@@ -77,61 +77,73 @@ if __name__ == "__main__":
             results = wikipedia.summary(query)
             print(results)
             speak(results)
-
-        elif 'open youtube' in query or "open video online" in query or "nassim" in query:
-            webbrowser.open("www.youtube.com")
-            speak("opening youtube")
+        #arduino orders    
         elif 'open light' in query or "light" in query or "turn the light" in query:
             speak("okay")
             arduino_orders1()
             time.sleep(2)
+        #openning youtube
+        elif 'open youtube' in query or "open video online" in query or "nassim" in query:
+            webbrowser.open("www.youtube.com")
+            speak("opening youtube")
+        #openning GitHub    
         elif 'open github' in query:
             webbrowser.open("https://www.github.com")
             speak("opening github")  
+        #openning Facebook
         elif 'open facebook' in query:
             webbrowser.open("https://www.facebook.com")
-            speak("opening facebook")      
+            speak("opening facebook")     
+        #openning Instagram 
         elif 'open instagram' in query:
             webbrowser.open("https://www.instagram.com")
             speak("opening instagram")    
+        #openning google    
         elif 'open google' in query:
             webbrowser.open("https://www.google.com")
             speak("opening google")
+        #openning a wesite called Phoenix    
         elif 'open phoenix' in query:
             webbrowser.open("https://phoenix-esgee.site/")
             speak("opening phoenix website")
+        #openning G-mail to check the inbox 
         elif 'open gmail' in query:
             webbrowser.open("https://mail.google.com")
             speak("opening google mail") 
-            
+        #openning Notion     
         elif 'open notion' in query:
             webbrowser.open("https://www.notion.so/") 
             speak("opening Notion !")  
-             
+        #openning Amazon     
         elif 'open amazon' in query or 'shop online' in query:
             webbrowser.open("https://www.amazon.com")
             speak("opening amazon")
+        #openning ChatGpt    
         elif 'open' and 'gpt' in query or 'open chatgpt' in query:
             webbrowser.open("https://chat.openai.com/")
             speak("opening chatgpt")
+        #Listening to Music from the folder in the path    
         elif 'music from pc' in query or "music" in query:
             speak("ok i am playing music")
             music_dir = 'D:\SnapTube Audio'
             musics = os.listdir(music_dir)
             os.startfile(os.path.join(music_dir,random.choice(musics)))
             
-
+        #Open a video from the specified path (watching a movie ...)
         elif 'video from pc' in query or "video" in query:
             speak("ok i am playing videos")
             video_dir = './video'
             videos = os.listdir(music_dir)
             os.startfile(os.path.join(video_dir,videos[0]))  
+        #Ending the programm (break)    
         elif 'good bye' in query:
             speak("good bye")
             exit()
+        #Shutting down the pc     
         elif "shut down" in query or  "close pc" in query:
             speak("shutting down")
-            os.system('shutdown -s') 
+            os.system('shutdown -s')
+        #Asking Jarvis how is he ...     
         elif "what\'s up" in query or 'how are you' in query:
             stMsgs = ['Just doing my thing!', 'I am fine!', 'Nice!', 'I am nice and full of energy','i am okey ! How are you']
             ans_q = random.choice(stMsgs)
@@ -142,31 +154,39 @@ if __name__ == "__main__":
                 speak('okey..')  
             elif 'not' in ans_take_from_user_how_are_you or 'sad' in ans_take_from_user_how_are_you or 'upset' in ans_take_from_user_how_are_you:
                 speak('oh sorry..')  
+        #Credits         
         elif 'make you' in query or 'created you' in query or 'develop you' in query:
             ans_m = " For your information Yacine Laribi Created me ! I give Lot of Thannks to Him "
             print(ans_m)
             speak(ans_m)
+        #Introduction of Jarvis ...    
         elif "who are you" in query or "about you" in query or "your details" in query:
             about = "I am Jarvis an A I based computer program but i can help you lot like a your close friend ! i promise you ! Simple try me to give simple command ! like playing music or video from your directory i also play video and song from web or online ! i can also entain you i so think you Understand me ! ok Lets Start "
             print(about)
             speak(about)
+        #Greeting    
         elif "hello" in query or "hello Jarvis" in query:
             hel = "Hello Yacine Laribi ! How May i Help you.."
             print(hel)
             speak(hel)
+        #Name    
         elif "your name" in query or "sweat name" in query:
             na_me = "Thanks for Asking my name my self ! Jarvis but they used to call me dababa"  
             print(na_me)
             speak(na_me)
+        #feelings    
         elif "you feeling" in query:
             print("feeling Very sweet after meeting with you")
             speak("feeling Very sweet after meeting with you") 
+        #i case the query is not detected !    
         elif query == 'none':
             continue 
+        #Exiting     
         elif 'exit' in query or 'abort' in query or 'stop' in query or 'bye' in query or 'quit' in query :
             ex_exit = 'Okay , i am exiting now'
             speak(ex_exit)
-            exit()    
+            exit()
+        #Googling something       
         elif 'google' in query :
             query=query.replace('google','')
             temp = query.replace(' ','+')
@@ -175,18 +195,22 @@ if __name__ == "__main__":
             print(res_g)
             speak(res_g)
             webbrowser.open(g_url+temp)
+        #Closing windows in the browser using (ctrl+w)    
         elif 'close' and 'window' in query:
             speak('okay')
             pyautogui.hotkey('ctrl', 'w')
+        #changing Window (tabilation)
         elif 'change' and 'window' in query:
             speak('okay')
             pyautogui.hotkey('alt', 'tab')
+        #If you are on a text editor or something !    
         elif 'write' in query:
             query=query.replace('write','')
             query.split()
             speak('writing'+query)
             pyautogui.hotkey(*query)
-            
+        #In case it doesn't understand you it searches in google 
+        #in reality we are going to add an LLM here for the response  
         else:
             temp = query.replace(' ','+')
             g_url="https://www.google.com/search?q="    
